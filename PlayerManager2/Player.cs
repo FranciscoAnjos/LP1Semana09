0797642a9387a9541f4a6f64ea34251db294c9f0
FranciscoAnjos <fcariaanjos@gmail.com>
2025-05-01 18:20:12 +0100

    create PlayerManager2

---
+++ PlayerManager2/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerManager2
+{
+    public class Player
+    {
+        public readonly string Name;
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
