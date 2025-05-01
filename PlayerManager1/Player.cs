e48686dc2bad13d790d028bae626908fbcb89dc1
FranciscoAnjos <fcariaanjos@gmail.com>
2025-05-01 18:12:09 +0100

    create player class

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerManager1
+{
+    public class Player
+    {
+        public readonly string Name;
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
