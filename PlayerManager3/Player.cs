5d608065868ac7c9c93253f98a858320ebcf0345
FranciscoAnjos <fcariaanjos@gmail.com>
2025-05-01 18:24:29 +0100

    compare players by score in descending order (higher scores come first)

---
+++ PlayerManager3/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlayerManager3
+{
+    public class Player: IComparable<Player>
+    {
+        public readonly string Name;
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        
+        public int CompareTo(Player other)
+        {
+            if (other == null) return -1;
+            return other.Score.CompareTo(this.Score); // descending order
+        }
+    }
+}
