ac245e9bd78addf50742957d3e6065940e856f57
FranciscoAnjos <fcariaanjos@gmail.com>
2025-05-01 18:29:09 +0100

    implement comparebyname class

--- PlayerManager4/CompareByName.cs
+++ PlayerManager4/CompareByName.cs
@@ -1,7 +1,24 @@
-﻿namespace PlayerManager4
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
 {
-    public class CompareByName
+    public class CompareByName: IComparer<Player>
     {
-        
+        private readonly bool _ascending;
+
+        public CompareByName(bool ascending)
+        {
+            _ascending = ascending;
+        }
+
+        public int Compare(Player x, Player y)
+        {
+            if (x == null || y == null) return 0;
+
+            int comparison = string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+
+            return _ascending ? comparison : -comparison;
+        }
     }
 }
